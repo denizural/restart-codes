78ed8f5d35d4020d55ec46dcaa25df3ef4455edc
Deniz Ural <denizural86@gmail.com>
2023-01-27 09:42:01 +0100

    adds print_json.py

---
+++ print_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""print_json.py:
+# Write a Python script that takes a JSON file as input and performs the following tasks:
+# a. Prints the entire JSON file
+# b. Prints the value of a specific key in the JSON file (you can choose any key)
+# c. Modifies the value of a specific key in the JSON file (you can choose any key)
+# d. Writes the modified JSON file to a new file
+"""
+
+import os
+import path
+import argparse
+import json
+import logging
+
+
